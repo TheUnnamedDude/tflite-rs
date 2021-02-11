1a38af17cd9c983fea5e15e39cfe6b78e02faaf5
Kevin Sillerud <kevin.sillerud@gmail.com>
2021-02-11 15:32:11 +0100

    WIP - support for custom op codes

--- src/interpreter/ops/builtin/resolver.rs
+++ src/interpreter/ops/builtin/resolver.rs
@@ -2,6 +2,7 @@ use std::mem;
 
 use crate::bindings::tflite as bindings;
 use crate::interpreter::op_resolver::OpResolver;
+use std::ffi::c_void;
 
 cpp! {{
     #include "tensorflow/lite/kernels/register.h"
@@ -11,6 +12,20 @@ cpp! {{
 
 pub struct Resolver {
     handle: Box<bindings::OpResolver>,
+}
+
+impl Resolver {
+    fn add_custom(&mut self, name: &str, registration: * const c_void) {
+        let name = ::std::ffi::CString::new(name).unwrap();
+        let name_ptr = name.as_ptr();
+        let resolver_ptr = self.handle.as_mut() as * mut _;
+        unsafe {
+            cpp!([resolver_ptr as "BuiltinOpResolver*", name_ptr as "const char*", registration as "TfLiteRegistration*"] -> () as "void" {
+                resolver_ptr->AddCustom(name_ptr, registration);
+                return;
+            })
+        }
+    }
 }
 
 impl Drop for Resolver {
